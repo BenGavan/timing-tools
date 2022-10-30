53ae8a8b04901eb11883a7c14bea11618d913a9f
Ben Gavan <BenGavan>
2022-10-30 20:20:04 +0000

    Add timing tool script

---
+++ timing-script/main.py
@@ -0,0 +1,30 @@
+import time
+
+
+def time_period(duration, interval):
+	print(f'duration: {duration}')
+	print(f'interval: {interval}')
+
+	for i in range(duration // interval):
+		print(i)
+		time.sleep(1)
+
+	print("*** End ***\a\a\a")
+
+
+def run():
+	duration = 10  # seconds between each counting/timing period
+	interval = 1   # seconds between each indicated increment
+
+	time_period(duration, interval)
+
+
+def main():
+	print("-----------")
+	print("Timing tool")
+	print("-----------")
+	run()
+
+
+if __name__ == '__main__':
+	main()
